b98e8474824d8359d36facf4732837de9a5393b1
hamdiranu <hamdira@sci.ui.ac.id>
2021-08-19 09:13:14 +0700

    finish part 1

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,20 +1,38 @@
 <template>
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <hello/>
-    <info/>
+  <div class="container">
+    <header>
+      <h1>Todo List</h1>
+    </header>
+    <ul>
+      <Task
+        v-for="task in getTasks()"
+        v-bind:key="task._id"
+        v-bind:task="task"
+      />
+    </ul>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
-import Info from './components/Info.vue'
+import Vue from "vue";
+import Task from "./components/Task.vue";
 
 export default {
   components: {
-    Hello,
-    Info,
+    Task,
   },
+  data() {
+    return {};
+  },
+  methods: {
+    getTasks() {
+      return [
+        { _id: 1, text: "This is task 1" },
+        { _id: 2, text: "This is task 2" },
+        { _id: 3, text: "This is task 3" }
+      ];
+    }
+  }
 }
 </script>
 
